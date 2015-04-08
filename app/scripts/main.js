564192c5c6c68b296b675bdbf60284f09e69f5bd
Stian Håklev <shaklev@gmail.com>
2015-04-08 12:37:00 -0400

    -m Working with threads (circles) and days, some metadata

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -15,32 +15,29 @@ var addMeta = function addMeta(data, meta) {
 
 function initialCanvas() {
 	var svg = d3.select("#d3").append("svg")
-	.attr("width",800)
-	.attr("height", App.forums_length * App.fontSize).append("g");
+	.attr("width", 200 + App.courseMeta.maxForumlength * 5)
+	.attr("height", App.forumsLength * App.fontSize).append("g");
 	return svg;
 }
 
-var translateString = function(d, i) {
-	var ret = "translate(" + 
-		(d.level * 10) + "," +
-		(i * App.fontSize + 20) + ")"; 
-	return ret;
-}
-
-
 var render = function(forums) {
-	console.log("Rendering day " + App.day);
-	var forums = App.forums['forums'];
-	var svg = App.svg;
+	var data = _.map(App.forums, function(x){
+		x['forumThreads'] = App.forumThreads[x.id]
+		return(x)
+	});
 
-	var labels = svg.selectAll(".label")
-	.data(forums, function(d) { return d.id });
+	// LABELS
+
+	var labels = App.svg.selectAll("g")
+	.data(data, function(d) { return d.id });
 
 	labels.enter()
-	.append("text")
+	.append("g")
+	.append('text')
 	.attr({
 		"font-size": App.fontSize,
-		transform: translateString
+		x: function (d, i) { return d.level * 10 },
+		y: function (d, i) {return i * App.fontSize + 20 }
 	})
 	.style("font-weight", function(d) { return +d.level < 4 ? "bold" : null })
 	.style("fill", function(d) { return +d.open_day > App.day ? "#CCCCCC" : null })
@@ -49,26 +46,84 @@ var render = function(forums) {
 	labels.style("fill", function(d) { return +d.open_day > App.day ? "#CCCCCC" : null });
 
 	labels.transition().delay(250).duration(1000)
-	.attr("transform", translateString);
+	.style("fill", function(d) { return +d.open_day > App.day ? "#CCCCCC" : null })
+	.attr({
+		"font-size": App.fontSize,
+		x: function (d, i) { return d.level * 10 },
+		y: function (d, i) {return i * App.fontSize + 20 }
+	});
 
 	labels.exit().transition()
-	.attr("transform", translateString)
 	.remove()
+
+	// CIRCLES
+
+	var circles = labels.selectAll("circle")
+	.data( function(d) { return d.forumThreads }, function(d) { return d[0] });
+
+	circles.enter()
+	.append("circle")
+	.attr({
+		"cx": function(d, i) { return (i * 6 + 200) },
+		"cy": function(d, i, j) { return (j * App.fontSize + 20)},
+		"r": function(d, i) { return (Math.sqrt( Math.log(d[1]) / Math.log(App.courseMeta["maxThreadlength"])) * 3) + 0.4 },
+		"fill": function(d, i) { return "#000000" }
+	});
+
+	circles.transition().delay(250).duration(1000)
+	.attr({
+		"cx": function(d, i) { return (i * 6 + 200) },
+		"cy": function(d, i, j) { return (j * App.fontSize + 20)},
+		"r": function(d, i) { return (Math.sqrt( Math.log(d[1]) / Math.log(App.courseMeta["maxThreadlength"])) * 3) + 0.4 },
+		"fill": function(d, i) { return "#000000" }
+	});
+
+	circles.exit().transition()
+	.remove()
+
 }
 
-console.log("Starting d/l");
-d3.json('http://localhost:5984/coursera_001_programming1/forums', 
-	function(err, data) {
-		if(err) { 
-			console.error(err) 
-		}
-		else {
-			App.forums = data;
-			App.forums_length = data['forums'].length;
-			App.svg = initialCanvas();
-			render()
-			App.day = 15;
-			render()
-		}
+App.rooturl = "http://localhost:5984/"
+App.course = "coursera_001_programming1"
+
+function urlBase(key) {
+	return App.rooturl + App.course + "/" + key
+}
+
+function initialLoad(err, forums, forumThreads, courseMeta) {
+	if(err) {
+		console.error(err)
+	} else
+	{
+		App.forums = forums['content'];
+		App.forumsLength = App.forums.length;
+
+		App.forumThreads = forumThreads['content'];
+		App.courseMeta = courseMeta['content'];
+
+		App.svg = initialCanvas();
+		render();
 	}
-);
+}
+
+function updateDay(err, data) {
+	if(err) {
+		console.error(err)
+	} else
+	{
+		App.forumThreads = data;
+		render();
+	}
+}
+
+
+function changeDay(day) {
+	App.day = day;
+    d3.json( urlBase("forumThreads_" + App.day), updateDay) 
+}
+
+queue()
+    .defer(d3.json, urlBase("forums")) 
+    .defer(d3.json, urlBase("forumThreads_1")) 
+    .defer(d3.json, urlBase("course_meta"))
+    .await(initialLoad); 
